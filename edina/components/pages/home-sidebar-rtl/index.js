d150765e2cb83de58c41d1e733cb5991123876e6
Steven Medina <smedina2494@gmail.com>
2025-02-10 21:29:01 -0500

    fixes parsing errors

--- edina/components/pages/home-sidebar-rtl/index.js
+++ edina/components/pages/home-sidebar-rtl/index.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FaMoon, FaSun } from "react-icons/fa";
-import { useState } from "react";
 import Sidebar from "../../header/Sidebar";
 import Hero from "../../hero/Hero";
 import About from "../../about/About";
@@ -13,176 +12,132 @@ import Address from "../../Address";
 import Map from "../../Map";
 import HeaderMobile from "../../header/HeaderMobile";
 
-// const EdinaHomeRtl = () => {
-const [isDark, setIsDark] = useState(false);
+const EdinaHomeRtl = () => {
+  const [isDark, setIsDark] = useState(false);
 
-useEffect(() => {
-  document.querySelector("body").classList.add("rtl");
-}, []);
+  useEffect(() => {
+    document.querySelector("body").classList.add("rtl");
+  }, []);
 
-const handleLabelClick = () => {
-  if (isDark) {
-    localStorage.setItem("theme-color", "theme-light");
-    document.querySelector("body").classList.add("theme-light");
-    document.querySelector("body").classList.remove("theme-dark");
-    setIsDark(false);
-  } else {
-    localStorage.setItem("theme-color", "theme-dark");
-    document.querySelector("body").classList.add("theme-dark");
-    document.querySelector("body").classList.remove("theme-light");
-    setIsDark(true);
-  }
+  const handleLabelClick = () => {
+    if (isDark) {
+      localStorage.setItem("theme-color", "theme-light");
+      document.querySelector("body").classList.add("theme-light");
+      document.querySelector("body").classList.remove("theme-dark");
+      setIsDark(false);
+    } else {
+      localStorage.setItem("theme-color", "theme-dark");
+      document.querySelector("body").classList.add("theme-dark");
+      document.querySelector("body").classList.remove("theme-light");
+      setIsDark(true);
+    }
+  };
+
+  return (
+    <div className={`home-light edina_tm_mainpart ${isDark ? "theme-dark" : ""}`}>
+      {/* Dark & Light Mode Switcher */}
+      <label className={`theme-switcher-label d-flex  ${isDark ? "active" : ""}`}>
+        <input type="checkbox" onClick={handleLabelClick} className="theme-switcher" />
+        <div className="switch-handle">
+          <i className="light-text" title="Switch to Dark">
+            <FaMoon />
+          </i>
+          <i className="dark-text" title="Switch to Light">
+            <FaSun />
+          </i>
+        </div>
+      </label>
+
+      {/* Header */}
+      <header className="header-area">
+        <div className="header-inner">
+          <HeaderMobile />
+        </div>
+      </header>
+      
+      <Sidebar />
+      <Hero />
+      <About />
+
+      {/* Services */}
+      <div className="edina_tm_services" id="service">
+        <div className="container">
+          <div className="edina_tm_title">
+            <h3>What I Do</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ipsum sit nibh amet egestas tellus.</p>
+          </div>
+          <Service />
+        </div>
+      </div>
+
+      {/* Portfolio */}
+      <div className="edina_tm_portfolio" id="portfolio">
+        <div className="container">
+          <div className="edina_tm_title">
+            <h3>Portfolio</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ipsum sit nibh amet egestas tellus.</p>
+          </div>
+          <Portfolio />
+        </div>
+      </div>
+
+      {/* Testimonials */}
+      <div className="edina_tm_testimonials" id="testimonial">
+        <div className="container">
+          <div className="edina_tm_title">
+            <h3>Testimonials</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ipsum sit nibh amet egestas tellus.</p>
+          </div>
+          <div className="list">
+            <ul>
+              <Testimonial />
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      {/* Blog */}
+      <div className="edina_tm_news" id="blog">
+        <div className="container">
+          <div className="edina_tm_title">
+            <h3>Blog</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ipsum sit nibh amet egestas tellus.</p>
+          </div>
+          <Blog />
+        </div>
+      </div>
+
+      {/* Contact */}
+      <div className="edina_tm_contact" id="contact">
+        <div className="container">
+          <div className="edina_tm_title">
+            <h3>Contact</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ipsum sit nibh amet egestas tellus.</p>
+          </div>
+          <div className="extra_info">
+            <Address />
+          </div>
+          <div className="mainpart">
+            <div className="left" data-aos="fade-right" data-aos-duration="1200" data-aos-delay="300">
+              <div className="title">
+                <p>
+                  I am always open to discussing product
+                  <br />
+                  <span> design work or partnerships.</span>
+                </p>
+              </div>
+              <div className="fields">
+                <Contact />
+              </div>
+            </div>
+            <div className="right" data-aos="fade-right" data-aos-duration="1200" data-aos-delay="400">
+              <Map />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
-
-return (
-  <div
-    className={`home-light edina_tm_mainpart ${isDark ? "theme-dark" : ""}`}
-  >
-    {/* Start Dark & Light Mode Swicher  */}
-    <label
-      className={`theme-switcher-label d-flex  ${isDark ? "active" : ""}`}
-    >
-      <input
-        type="checkbox"
-        onClick={handleLabelClick}
-        className="theme-switcher"
-      />
-      <div className="switch-handle">
-        <i className="light-text" title="Switch to Dark">
-          <FaMoon />
-        </i>
-        <i className="dark-text" title="Switch to Light">
-          <FaSun />
-        </i>
-      </div>
-    </label>
-    {/* End Dark & Light Mode Swicher  */}
-
-    <header className="header-area">
-      <div className="header-inner">
-        <HeaderMobile />
-      </div>
-    </header>
-    {/* End mobile-header */}
-
-    <Sidebar />
-    {/* End Header */}
-    <Hero />
-    {/* End Hero */}
-    <About />
-    {/* End Hero */}
-    <div className="edina_tm_services" id="service">
-      <div className="container">
-        <div className="edina_tm_title">
-          <h3>What I Do</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            ipsum sit nibh amet egestas tellus.
-          </p>
-        </div>
-        {/* End edian_tm_title */}
-        <Service />
-      </div>
-    </div>
-    {/* End Services */}
-
-    <div className="edina_tm_portfolio" id="portfolio">
-      <div className="container">
-        <div className="edina_tm_title">
-          <h3>Portfolio</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            ipsum sit nibh amet egestas tellus.
-          </p>
-        </div>
-        {/* End edian_tm_title */}
-        <Portfolio />
-      </div>
-    </div>
-    {/* End Portfolio */}
-
-    <div className="edina_tm_testimonials" id="testimonial">
-      <div className="container">
-        <div className="edina_tm_title">
-          <h3>Testimonials</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            ipsum sit nibh amet egestas tellus.
-          </p>
-        </div>
-        {/* End edian_tm_title */}
-        <div className="list ">
-          <ul>
-            <Testimonial />
-          </ul>
-        </div>
-      </div>
-    </div>
-    {/* End Testimonial */}
-
-    <div className="edina_tm_news" id="blog">
-      <div className="container">
-        <div className="edina_tm_title">
-          <h3>Blog</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            ipsum sit nibh amet egestas tellus.
-          </p>
-        </div>
-        {/* End edian_tm_title */}
-        <Blog />
-      </div>
-    </div>
-    {/* End Blog */}
-
-    <div className="edina_tm_contact" id="contact">
-      <div className="container">
-        <div className="edina_tm_title">
-          <h3>Contact</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            ipsum sit nibh amet egestas tellus.
-          </p>
-        </div>
-        <div className="extra_info">
-          <Address />
-        </div>
-        {/* End Address Info */}
-        <div className="mainpart">
-          <div
-            className="left"
-            data-aos="fade-right"
-            data-aos-duration="1200"
-            data-aos-delay="300"
-          >
-            <div className="title">
-              <p>
-                I am always open to discussing product
-                <br />
-                <span> design work or partnerships.</span>
-              </p>
-            </div>
-            {/* End title */}
-            <div className="fields">
-              <Contact />
-            </div>
-            {/* End Contact Form */}
-          </div>
-          <div
-            className="right"
-            data-aos="fade-right"
-            data-aos-duration="1200"
-            data-aos-delay="400"
-          >
-            <Map />
-          </div>
-          {/* End Map */}
-        </div>
-      </div>
-    </div>
-    {/* /CONTACT */}
-  </div>
-);
-// };
 
 export default EdinaHomeRtl;
