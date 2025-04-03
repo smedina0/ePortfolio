54272620b050ebcee8aab21a8c0ec1785c44be5d
Steven Medina <smedina2494@gmail.com>
2025-04-03 00:12:19 -0400

    last min changes

--- edina/data/testimonial.js
+++ edina/data/testimonial.js
@@ -9,14 +9,14 @@ module.exports = [
   {
     img: "",
     desc: `These people really know what they are doing! Great customer support availability and supperb kindness. I am very happy that I've purchased this liscense!!!`,
-    name: "Fabian Gattuzo",
+    name: "My Journey",
     designation: "Photographer",
     delayAnimation: "100",
   },
   {
     img: "",
     desc: `These people really know what they are doing! Great customer support availability and supperb kindness. I am very happy that I've purchased this liscense!!!`,
-    name: "Alizee Bonita",
+    name: "Incorporating Disabilitity Studies to improve my role",
     designation: "App Developer",
     delayAnimation: "200",
   },
