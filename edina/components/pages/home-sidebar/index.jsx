cfb2eb84b62d3b2bfd481696b70d64fcc4bc988c
Steven Medina <smedina2494@gmail.com>
2025-04-02 21:51:16 -0400

    program reflection

--- edina/components/pages/home-sidebar/index.jsx
+++ edina/components/pages/home-sidebar/index.jsx
@@ -25,7 +25,7 @@ const EdinaHomeSidebar = () => {
     } else {
       document.body.style.overflow = 'auto';
     }
-    
+
     return () => {
       document.body.style.overflow = 'auto';
     };
@@ -39,8 +39,8 @@ const EdinaHomeSidebar = () => {
         </div>
       </header>
 
-      <div 
-        className={`menu-overlay ${isMenuOpen ? 'active' : ''}`} 
+      <div
+        className={`menu-overlay ${isMenuOpen ? 'active' : ''}`}
         onClick={() => setIsMenuOpen(false)}
       />
 
@@ -64,11 +64,28 @@ const EdinaHomeSidebar = () => {
         <div className="container">
           <div className="edina_tm_title">
             <h3>Program Reflection</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-              ipsum sit nibh amet egestas tellus.
-            </p>
           </div>
+          <p className="largerText">
+            My time in this Master&apos;s in Disability Studies program has been
+            a profound exploration of disability as a complex experience. The
+            program had a particularly curated selection of courses that has
+            provided me with several perspectives that extend beyond traditional
+            educational teachings. In courses like Psyhosocial, Cultural and
+            Political Aspects of Disability (DSAB 601) and Dsiability Studies &
+            the Humanities (DSAB 621), we challenged societal narratives and
+            understood disability through intersectional and humanistic lenses.
+            Alongside the scholarly and theoretical perspectives were courses
+            related to the practical applciations of disabilities studies like
+            Leadership in Disability Services Agencies (DSAB 624) and Disability
+            Law & Policy (DSAB 626). The variety of the curriculum has prepared
+            me to be an informed, empathetic advocate and professional in the
+            disability studies field.
+          </p>
+          <br></br>
+          <p className="largerText">
+            I have been in the field of digital accessibility for over 6 years.
+            In my time as an accessibility professional,I have learned the ins and outs of document accessibility (Microsoft Office/Adobe Suite), annotations in design accessibility, accessibility quality assurance/accessibility testing, and accessibility development. In my time in digital accessibility, I learned the "trade" of digital accessibility, but I realized that I was lacking in terms of the social, political, administrative, and legal aspects of disability. This lack of knowledge was disappointing to me. Reflecting on this program, I am very grateful for what I have learned and the knowledge, resources, and conversations this program has provided me with. I have taken what I learned in this course and have applied it to my work, my conversations, and my advocacy for people with disabilities. I am much more confident when talking about usability and when explaining the importance of what I do with people in related fields (designers/developers/testers/product owners/etc).
+          </p>
           {/* <Portfolio /> */}
         </div>
       </div>
@@ -108,12 +125,14 @@ const EdinaHomeSidebar = () => {
           <div className="">
             <h3>Contact</h3>
             <p>
-            I am passionate about making digital spaces work for everyone, drawing from my background in disability studies and hands-on tech experience. My goal is to break down digital barriers and create online experiences that are not just accessible, but actually provide great user experiences.
+              I am passionate about making digital spaces work for everyone,
+              drawing from my background in disability studies and hands-on tech
+              experience. My goal is to break down digital barriers and create
+              online experiences that are not just accessible, but actually
+              provide great user experiences.
             </p>
           </div>
-          <div className="extra_info">
-            {/* <Address /> */}
-          </div>
+          <div className="extra_info">{/* <Address /> */}</div>
           <div className="mainpart">
             <div
               className="left"
@@ -123,7 +142,8 @@ const EdinaHomeSidebar = () => {
             >
               <div className="title">
                 <p>
-                  While I am not open to work, I am open to conversations and networking regarding
+                  While I am not open to work, I am open to conversations and
+                  networking regarding
                   <br />
                   <span>accessible design, development, and testing.</span>
                 </p>
