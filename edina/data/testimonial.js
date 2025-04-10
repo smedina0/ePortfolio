35c55d5480041ced4d5aaaa3b51538b40ae7f0c3
Steven Medina <smedina2494@gmail.com>
2025-04-10 01:42:05 -0400

    Updates unique section

--- edina/data/testimonial.js
+++ edina/data/testimonial.js
@@ -29,9 +29,31 @@ module.exports = [
           </dd>
         </dl>
         <br></br>
+        <h4>How to Incorporate the Above Rules</h4>
+        <p>
+"Shifting Left" is an important concept in the product life cycle as it relates to accessiblilty. This means getting in early and considering accessibility in the early stges. For perceivability, focus on ensuring content and functionality are available across a variety of senosory inputs. For sighted users, ensure that we use enough color contrast and allow for responsiveness (such as screen resizing). For screen reader users, ensure all images have appropriate text alternatives and that all controls have appropriate labels. For users who are hearing impaired, provide transcripts and captions. For users with 
+<br>
+        </br>
+        <br>
+        </br>
+        To ensure your websites are operable, allow for funcionality through keyboards for users who have motor disabilities or who just generally prefer keyboard shortcuts. Time based mechanisms should be avoided as much as possible and also keep in mind to have alternatives for gestures. Ideally touch targets are at least 44 x 44 pixels.
+        </p>
+        <br>
+        </br>
+        
+        <p>
+          Understandability is one of those weird ones where the accessibility of it comes mostly from generally good usability practices. For example, using plain language and having consistent navigations/interaction patterns is important. Additionally, having clear error messages, consistent form labels, and supplementary illustrations, icons, or diagrams may help improve accessibility.
+        </p>
+        <br>
+        </br>
+        <p>
+          When it comes to robustness, we should follow the HTML5 web standards and ARIA recommendations. THis will ensure that our code is compatible with current and future assistive technology support. Testing content with screen readers and other devices will also ensure that we can catch accessibility challenges.
+        </p>
+        <br>
+        </br>
       </>
     ),
-    
+
     name: "What is Digital Accessibility?",
     designation: "",
     delayAnimation: "",
@@ -40,34 +62,49 @@ module.exports = [
     img: "",
     desc: (
       <>
-        <p>
-          Digital accessibility is the idea of designing, developing, testing, and maintaining products and software so that they are usable for everyone, with an extra emphasis on being usable by people with disabilities. The pillars of digital accessibility are perceivability, operability, understandability, and robustness (POUR).
-        </p>
         <br></br>
         <p>
-          Perceivability requires that content and functionality are capable of being experienced by everyone through a medium that can convey information whether it be visual, auditory, or tactile.
+Despite being a legal requirement, it is important and fundamental to create inclusive digital experiences that work for everyone. When websites are easy to use for people with disabilities, they become easier for everyone to use. It&apos;s like how curb cuts (those ramps at sidewalk corners) were made for wheelchairs but help people with bikes, strollers, and rolling luggage too. 
+<br></br>
+          <br></br>
+          About 1 in 4 people have some type of disability. That&apos;s a lot of people who might not be able to use your website if it&apos;s not accessible. By making your site work for them, you&apos;re not shutting out a huge part of your audience. Remember, if you live long enough, you will probably identify as having a disability one day too.
+          <br></br>
+          <br></br>
+          Companies benefit from accessible websites too. They get more customers, avoid getting sued, and look better to the public. It&apos;s literally in their best interest to morally create usable websites for everyone.
+          <br></br>
+          <br></br>
+          Accessibility also helps people in tough situations, like someone using their phone in bright sunlight who needs good contrast, or someone with a broken arm who can&apos;t use a mouse. The same features that help people with disabilities help everyone at different times.
+          <br></br><br></br>
+          In the end, making websites accessible is just fair. Everyone should be able to use the internet, which has become a necessary part of modern life.
+
+          <br>
+          </br>
+          <br>
+          </br>
         </p>
       </>
     ),
-    name: "My Journey",
-    designation: "Photographer",
-    delayAnimation: "100",
+    name: "Why Digital Accessibility Matters",
+    designation: "",
+    delayAnimation: "",
   },
   {
     img: "",
     desc: (
       <>
-        <p>
-          Digital accessibility is the idea of designing, developing, testing, and maintaining products and software so that they are usable for everyone, with an extra emphasis on being usable by people with disabilities. The pillars of digital accessibility are perceivability, operability, understandability, and robustness (POUR).
-        </p>
-        <br></br>
-        <p>
-          Perceivability requires that content and functionality are capable of being experienced by everyone through a medium that can convey information whether it be visual, auditory, or tactile.
-        </p>
+       <br>
+       </br>
+<p>
+
+It took me a while, but I&apos;ve figured out how to use what I learned in disability studies in my digital accessibility job. The biggest benefit is that I can be a better advocate for people with disabilities. I know I can&apos;t understand every disability, but I can talk about my own experiences and share what I&apos;ve learned about others.
+I now have real examples that help me show my design and development teammates why accessibility matters for real people. It&apos;s much easier to convince my team to make something accessible when I can give specific examples of challenges people face, rather than just saying &quot;we have to do this for legal reasons.&quot; When I tell stories about real problems, my coworkers actually get it.
+When I bring up these real-life examples early in our design process, we make better products from the start instead of trying to fix them later. My background also helps me spot when our designs assume everyone uses technology the same way, which isn&apos;t true.
+This people-first approach not only creates better websites and apps but also teaches my coworkers to think more inclusively as we work together.
+</p>
       </>
     ),
     name: "Incorporating Disabilitity Studies to improve my role",
-    designation: "App Developer",
-    delayAnimation: "200",
+    designation: "",
+    delayAnimation: "",
   },
 ];
